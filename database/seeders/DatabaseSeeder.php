4661d60c335ee03359151cbc0d3c35adb1637dc1
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2022-11-03 17:16:38 +0800

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            CampTableSeeder::class,
+            CampBenefitTableSeeder::class,
+            AdminUserSeeder::class,
+        ]);
     }
 }
