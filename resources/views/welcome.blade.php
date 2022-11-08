4454d1af682466a8a898dd246f71e5e2766d5d9a
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2022-11-08 15:23:40 +0800

    integeration landing page, checkout & my dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -263,7 +263,7 @@
                                 <div class="clear"></div>
                             </div>
                             <p>
-                                <a href="{{route('checkout')}}" class="btn btn-master btn-primary w-100 mt-3">
+                                <a href="{{route('checkout.create', 'gila-belajar')}}" class="btn btn-master btn-primary w-100 mt-3">
                                     Take This Plan
                                 </a>
                             </p>
@@ -309,7 +309,7 @@
                                 <div class="clear"></div>
                             </div>
                             <p>
-                                <a href="{{route('checkout')}}" class="btn btn-master btn-secondary w-100 mt-3">
+                                <a href="{{route('checkout.create', 'baru-mulai')}}" class="btn btn-master btn-secondary w-100 mt-3">
                                     Start With This Plan
                                 </a>
                             </p>
