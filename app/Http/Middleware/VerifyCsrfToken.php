ee3d8d5715d043e3e36512450b738674e4cf86e2
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2022-11-14 22:40:07 +0800

    fixing midtrans snap redirect and callback

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,10 +7,10 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array<int, string>
-     */
+    * The URIs that should be excluded from CSRF verification.
+    *
+    * @var array<int, string>
+    */
     protected $except = [
         //
     ];
