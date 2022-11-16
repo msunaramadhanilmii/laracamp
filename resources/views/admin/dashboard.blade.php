c2988a51dbfcc48d80404968223ca38b0e398105
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2022-11-16 08:06:49 +0800

    feat: discount management admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-8 offset-2">
-                <div class="card">
+                <div class="card mt-3">
                     <div class="card-header">
                         My Camps
                     </div>
